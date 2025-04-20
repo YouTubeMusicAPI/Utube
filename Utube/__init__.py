f95db5a8ee71a17654ff1ebebab025a9e68df941
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2025-04-21 02:02:59 +0530

    Update __init__.py

--- Utube/__init__.py
+++ Utube/__init__.py
@@ -2,4 +2,4 @@ from .Extractor import YouTubeExtractor
 from .Downloader import Downloader
 from .Format_selector import FormatSelector
 from .Post_processing import PostProcessor
-from .Searcher import Search
+from .Searcher import search
